b9e9eec0f95bb53a2d41a9a462dc16d99c0f3e3d
Heithvaldr <heithvaldr@gmail.com>
2014-12-16 22:23:18 +0200

    completing carousel slider and alignung footer to the bottom

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-$(".link").each(function () {
+$( document ).ready(function() {
+
+ console.log('ready');
+
+  $(".link").each(function () {
     text = $(this).text();
     if (text.length > 150) {
         $(this).html(text.substr(0, 150));
     }
+ });
+
+});
+$(document).ready(function() {
+
+$("#slider").owlCarousel();
+
+});
+
+$(".owl-carousel").owlCarousel({
+/* navigation: true,*/
+slideSpeed: 300,
+paginationSpeed: 400,
+singleItem: true,
+autoHeight: true,
+autoPlay: true
 });
